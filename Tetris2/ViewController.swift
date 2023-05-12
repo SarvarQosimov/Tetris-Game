d4eddecaa133ea51ad4c09d93abf5bcea48d0658
Sarvar <sarvarqosimov094@gmail.com>
2023-05-12 15:10:25 +0500

    Initial Commit

---
+++ Tetris2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tetris2
+//
+//  Created by Sarvar Qosimov on 12/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
